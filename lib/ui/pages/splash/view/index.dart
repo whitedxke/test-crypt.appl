3221da3a108ea9e56831b75ce4368413ea05b6e0
whitedxke <whitedxke@gmail.com>
2024-01-25 01:14:21 +0300

    changed the design.

--- lib/ui/pages/splash/view/index.dart
+++ lib/ui/pages/splash/view/index.dart
@@ -5,10 +5,14 @@ export 'package:flutter/material.dart';
 // ---- Packages.
 
 export 'package:flutter_screenutil/flutter_screenutil.dart';
+export 'package:flutter_svg/svg.dart';
 export 'package:flutter_bloc/flutter_bloc.dart';
 
 // ---- Components.
 
+export '../../../../core/resources/image_resource.dart';
+export '../../../../core/resources/text/text_line_height_resource.dart';
+export '../../../../core/resources/text/text_style_resource.dart';
 export '../../../../core/resources/color_resource.dart';
 export '../../../../core/resources/strings/string_resource.dart';
 export '../../../widgets/parent_widget.dart';
