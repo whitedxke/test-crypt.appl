767900f29319bc4d907a506af54435caecb00482
whitedxke <whitedxke@gmail.com>
2024-02-10 11:10:37 +0300

    Changed observer, and added a new methods.

--- lib/observer.dart
+++ lib/observer.dart
@@ -2,6 +2,30 @@ import 'index.dart';
 
 class ApplicationBlocObserver extends BlocObserver {
   @override
+  void noSuchMethod(Invocation invocation) {
+    super.noSuchMethod(invocation);
+    print('ApplicationBlocObserver. Such method: $invocation');
+  }
+
+  @override
+  void onChange(BlocBase bloc, Change change) {
+    super.onChange(bloc, change);
+    print('ApplicationBlocObserver. Change: $change');
+  }
+
+  @override
+  void onClose(BlocBase bloc) {
+    super.onClose(bloc);
+    print('ApplicationBlocObserver. Close: $bloc');
+  }
+
+  @override
+  void onCreate(BlocBase bloc) {
+    super.onCreate(bloc);
+    print('ApplicationBlocObserver. Create: $bloc');
+  }
+
+  @override
   void onEvent(Bloc bloc, Object? event) {
     super.onEvent(bloc, event);
     print('ApplicationBlocObserver. Event: $event');
